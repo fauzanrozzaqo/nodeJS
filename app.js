8fb7ac66b926cf84e33a12860df3f365cbb2037d
fauzanrozzaqo <frkusuma@gmail.com>
2020-01-08 16:21:18 +0700

    menggunakan express lib

--- app.js
+++ app.js
@@ -1,18 +1,20 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 
+app.use('/public',express.static(path.join(__dirname, 'static')));
 app.get('/',(req,res)=>{
-    res.send('Hello World');
+    res.sendFile(path.join(__dirname,'static','index.html'));
 });
-
-app.get('/example', (req,res)=>{
-    res.send('hitting example route');
-});
-
-app.get('/example/:name/:age', (req, res)=>{
-    console.log(req.params);
-    console.log(req.query);
-    res.send(req.params.name + " : " + req.params.age);
-});
-
 app.listen(3000);
+// app.get('/example', (req,res)=>{
+//     res.send('hitting example route');
+// });
+
+// menggunakan query dan params method
+// app.get('/example/:name/:age', (req, res)=>{
+//     console.log(req.params);
+//     console.log(req.query);
+//     res.send(req.params.name + " : " + req.params.age);
+// });
+
