86a943df1dda0e38f7b568044685554cbd236b69
fauzanrozzaqo <frkusuma@gmail.com>
2020-01-09 15:32:40 +0700

    menggunakan serve static express

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-app.use('/public',express.static(path.join(__dirname, 'static')));
+app.use('/public',express.static('static'));
 app.get('/',(req,res)=>{
     res.sendFile(path.join(__dirname,'static','index.html'));
 });
