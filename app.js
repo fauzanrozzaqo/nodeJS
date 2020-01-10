164822ae28eeb0d31438be4abb51df153a470f9d
fauzanrozzaqo <frkusuma@gmail.com>
2020-01-10 10:08:32 +0700

    install bodyparser untuk penggunaan parser form

--- app.js
+++ app.js
@@ -1,10 +1,22 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+const bodyParser = require('body-parser');
 
+//penggunaan serve static in node js
 app.use('/public',express.static('static'));
+//membuat page kita dapat melewati url encoded
+app.use(bodyParser.urlencoded({extended: false}));
+
 app.get('/',(req,res)=>{
     res.sendFile(path.join(__dirname,'static','index.html'));
+});
+
+app.post('/', (req,res)=>{
+    //melihat apakah data dari form di index.html terbaca atau tidak
+    console.log(req.body);
+    //database work here
+    res.send('succesfully posted data');
 });
 app.listen(3000);
 // app.get('/example', (req,res)=>{
